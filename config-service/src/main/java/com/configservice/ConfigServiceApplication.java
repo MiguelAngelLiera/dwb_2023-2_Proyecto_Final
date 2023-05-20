bc7e18103f794911a0edc9d5d5469c1ac19cddf5
MiguelAngelLiera <miguelliera@ciencias.unam.mx>
2023-05-20 02:58:01 -0600

    Cambios a la configuracion

--- config-service/src/main/java/com/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/configservice/ConfigServiceApplication.java
@@ -1,7 +1,8 @@
-package com.example.configservice;
+package com.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
--- config-service/src/test/java/com/configservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/com/configservice/ConfigServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.configservice;
+package com.configservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
